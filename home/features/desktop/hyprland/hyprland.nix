23e71a82fdbb207e419cef4cbca963b55bfd8434
Haru02w <haru02w@protonmail.com>
2024-01-10 10:48:45 -0300

    stable hyprland

--- home/features/desktop/hyprland/hyprland.nix
+++ home/features/desktop/hyprland/hyprland.nix
@@ -10,7 +10,6 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     plugins = [
       inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
     ];
