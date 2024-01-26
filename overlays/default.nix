61f77cd9234040a07a06235e3c27ead55d33baef
Haru02w <haru02w@protonmail.com>
2024-01-26 01:07:53 -0300

    update: flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,5 @@
 { inputs, ... }: {
   nur = inputs.nur.overlay;
-  devenv = inputs.devenv.overlays.default;
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
