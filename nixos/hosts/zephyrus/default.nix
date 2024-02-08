ad0ce89ca2873e6c409c5c847d72841f9c58c6e1
Haru02w <haru02w@protonmail.com>
2024-02-08 08:29:17 -0300

    some bootloader stuff

--- nixos/hosts/zephyrus/default.nix
+++ nixos/hosts/zephyrus/default.nix
@@ -1,6 +1,9 @@
-{pkgs, outputs, ...}:{
+{outputs, ...}:{
   imports = [
     (builtins.attrValues outputs.nixosModules)
     ./disko.nix
+    ./hardware-configuration.nix
+    ../common/global
   ];
+  
 }
