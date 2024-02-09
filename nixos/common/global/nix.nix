c46277504db220d8d48428573cd4aeda8672f025
Haru02w <haru02w@protonmail.com>
2024-02-09 00:55:52 -0300

    huge advancement

---
+++ nixos/common/global/nix.nix
@@ -0,0 +1,32 @@
+{inputs,outputs, lib, ...}:
+{
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config.allowUnfree = true;
+    config.allowUnfreePredicate = _: true;
+  };
+
+  nix = {
+    optimise.automatic = true;
+
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      system-features = [ "kvm" "big-parallel" "nixos-test" ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "*-*-* 00/12:00:00";
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # Add nixpkgs input to NIX_PATH
+    # This lets nix2 commands still use <nixpkgs>
+    nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ];
+  };
+}
