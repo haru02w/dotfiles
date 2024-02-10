63a16e6c9d27d5c5724316235e045c82a9c303a2
Haru02w <haru02w@protonmail.com>
2024-02-10 14:00:03 -0300

    test vm added (incomplete)

---
+++ nixos/testvm/default.nix
@@ -0,0 +1,32 @@
+{ outputs, ... }: {
+  imports = (builtins.attrValues outputs.nixosModules) ++ [
+    # settings
+    ../common/global
+    ../common/settings/locale_n_timezone.nix
+    ../common/settings/sops.nix
+    ../common/settings/impermanence.nix
+    ../common/users/haru02w.nix
+
+    #programs
+    ../common/programs/hyprland.nix
+    ../common/programs/pipewire.nix
+    ../common/programs/tuigreet.nix
+    ../common/programs/udisks2.nix
+
+    # host specific
+    ./hardware-configuration.nix
+    ./disko.nix
+  ];
+  boot.loader.grub.device = "/dev/vda";
+
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
+
+  #ignore lid close
+  services.logind.extraConfig = ''
+    HandleLidSwitch=ignore
+    HandleLidSwitchExternalPower=ignore
+  '';
+
+  networking.hostName = "zephyrus";
+}
---
+++ nixos/testvm/disko.nix
@@ -0,0 +1,52 @@
+let disk = "/dev/vda";
+in {
+  disko.devices.disk.nvme = {
+    type = "disk";
+    device = disk;
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "defaults" ];
+          };
+        };
+        luks = {
+          size = "254412152832"; # half-disk
+          content = {
+            type = "luks";
+            name = "crypted";
+            settings.allowDiscards = true;
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "/swap" = {
+                  mountpoint = "/.swapvol";
+                  swap.swapfile.size = "16G";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
