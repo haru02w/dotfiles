3f93788b819e913a5a14844b8e4a361e3a8dad30
Haru02w <haru02w@protonmail.com>
2024-02-10 13:46:37 -0300

    add more zephyrus modules (not tested yet)

--- nixos/zephyrus/default.nix
+++ nixos/zephyrus/default.nix
@@ -1,14 +1,27 @@
 { inputs, outputs, ... }: {
   imports = (builtins.attrValues outputs.nixosModules) ++ [
+    # settings
+    ../common/global
+    ../common/bootloader/uefi_systemd-boot.nix
+    ../common/settings/locale_n_timezone.nix
+    ../common/settings/sops.nix
+    ../common/settings/impermanence.nix
+    ../common/users/haru02w.nix
+
+    #programs
+    ../common/programs/hyprland.nix
+    ../common/programs/pipewire.nix
+    ../common/programs/tuigreet.nix
+    ../common/programs/udisks2.nix
+
+    # host specific
     ./hardware-configuration.nix
     ./disko.nix
-    ../common/bootloader/uefi_systemd-boot.nix
-    ../common/global
-    ../common/sops.nix
-    ../common/users/haru02w.nix
     ./laptop.nix
+    ./nvidia.nix
     inputs.nixos-hardware.nixosModules.asus-zephyrus-ga401
   ];
+
   services.tailscale.enable = true;
 
   #ignore lid close
