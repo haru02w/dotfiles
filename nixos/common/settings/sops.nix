2b86029a3366352dbbc138136d679de886dbe013
Haru02w <haru02w@protonmail.com>
2024-02-10 21:51:29 -0300

    update: flake.lock

--- nixos/common/settings/sops.nix
+++ nixos/common/settings/sops.nix
@@ -1,12 +1,6 @@
-{ config, lib, ... }:
 # WARN: put private key at this location
 let
-  privateKey = ''
-    ${lib.optionalString
-      (config.environment.persistence ? "/persist")
-      "/persist"
-    }/etc/sops/age/keys.txt
-  '';
+  privateKey = "/etc/sops/age/keys.txt";
 in {
   sops = {
     defaultSopsFile = ../../secrets/accounts.yaml;
