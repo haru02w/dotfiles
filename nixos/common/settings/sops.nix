c59eb24b55311891a390b649bbe00f9e69e02cc0
Haru02w <haru02w@protonmail.com>
2024-02-12 10:02:28 -0300

    idk

--- nixos/common/settings/sops.nix
+++ nixos/common/settings/sops.nix
@@ -1,11 +1,13 @@
 { lib, config, ... }:
-# WARN: put private key at this location
-let hasImpermanence = config.environment.persistence ? "/persist";
+let
+  # WARN: put private key at this location
+  privateKey =
+    "${lib.optionalString hasImpermanence "/persist"}/etc/sops/age/keys.txt";
+  hasImpermanence = config.environment.persistence ? "/persist";
 in {
   sops = {
     defaultSopsFile = ../../secrets/accounts.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile =
-      "${lib.optionalString hasImpermanence "/persist"}/etc/sops/age/keys.txt";
+    age.keyFile = privateKey;
   };
 }
