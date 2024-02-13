cebf5c8ff6666138d6c59f8b6541249da77e811e
Haru02w <haru02w@protonmail.com>
2024-02-13 11:51:50 -0300

    fix3

--- home/haru02w/common/sops.nix
+++ home/haru02w/common/sops.nix
@@ -4,17 +4,17 @@ in {
   home.packages = with pkgs; [ sops ];
 
   sops = {
-    defaultSopsFile = ../../../secrets/ssh-keys.yaml;
+    defaultSopsFile = ../../../secrets/accounts.yaml;
     defaultSopsFormat = "yaml";
     age.sshKeyPaths = keys;
 
     secrets.rsa_id = {
-      sopsFile = ../../secrets/ssh-keys.yaml;
+      sopsFile = ../../../secrets/ssh-keys.yaml;
       path = "${config.home.homeDirectory}/.ssh/id_rsa";
     };
 
     secrets.rsa_id_pub = {
-      sopsFile = ../../secrets/ssh-keys.yaml;
+      sopsFile = ../../../secrets/ssh-keys.yaml;
       path = "${config.home.homeDirectory}/.ssh/id_rsa.pub";
     };
   };
