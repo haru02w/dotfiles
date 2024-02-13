010e5be0596e77c108922c0a8ef89d10e35a5f32
Haru02w <haru02w@protonmail.com>
2024-02-12 21:56:35 -0300

    add ssh path to impermanence

--- nixos/common/settings/impermanence.nix
+++ nixos/common/settings/impermanence.nix
@@ -10,9 +10,9 @@
         "/var/log" # logs
         "/var/lib/tailscale" # tailscale login
         "/etc/asusd" # asusctl state
+        "/etc/ssh"
       ];
       files = [
-        "/etc/sops/age/keys.txt" # passwords stuff
         "/root/.local/share/nix/trusted-settings.json"
       ];
     };
