7aa7b44f544a996e8b35d25d5e03f22843cab137
Haru02w <haru02w@protonmail.com>
2024-02-13 11:49:37 -0300

    fix

--- home/haru02w/common/sops.nix
+++ home/haru02w/common/sops.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 let
-  key = "/etc/ssh/ssh_host_ed25519_key";
+  keys = ["/etc/ssh/ssh_host_ed25519_key"];
 in {
   home.packages = with pkgs; [ sops ];
 
   sops = {
     defaultSopsFile = ../../secrets/accounts.yaml;
     defaultSopsFormat = "yaml";
-    age.sshKeyPaths = key;
+    age.sshKeyPaths = keys;
 
     secrets.rsa_id = {
       sopsFile = ../../secrets/ssh-keys.yaml;
