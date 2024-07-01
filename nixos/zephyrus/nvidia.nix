baf80e26c8380b03635514eebde192166e7980a1
Haru02w <haru02w@protonmail.com>
2024-07-01 08:56:09 -0300

    fix dri device on hyprland

--- nixos/zephyrus/nvidia.nix
+++ nixos/zephyrus/nvidia.nix
@@ -1,8 +1,16 @@
-{
+{ config, ... }: {
   hardware.nvidia = {
     powerManagement.enable = true;
     powerManagement.finegrained = true;
     dynamicBoost.enable = true;
   };
-  environment.sessionVariables.WLR_DRM_DEVICES = "/dev/dri/card0";
+  environment.etc = {
+    "amdcard" = { source = "/dev/dri/by-path/pci-0000:04:00.0-card"; };
+    "nvicard" = { source = "/dev/dri/by-path/pci-0000:01:00.0-card"; };
+  };
+  environment.sessionVariables.WLR_DRM_DEVICES = ''
+    /etc/${config.environment.etc."amdcard".target}:/etc/${
+      config.environment.etc."nvicard".target
+    }
+  '';
 }
