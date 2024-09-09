2b30c5d6531afe0604c2d687e041b0f0dcc05502
Haru02w <haru02w@protonmail.com>
2024-09-09 08:58:27 -0300

    fix deploy script

--- hosts/testvm/nixos/deploy.sh
+++ hosts/testvm/nixos/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- -m disko --arg device '"/dev/vda"' ./disko.nix && \
-sudo nixos-generate-config --no-filesystems --root /mnt --show-hardware-config > ./hardware-configuration.nix
+sudo nixos-generate-config --no-filesystems --root /mnt --show-hardware-config > ./hardware-configuration.nix && \
+git add ./hardware-configuration.nix && \
 yes | sudo nixos-install --root /mnt --flake .#testvm
