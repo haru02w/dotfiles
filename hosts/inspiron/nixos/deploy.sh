cbe9de345cf18bf4823d0d936d6917450821e6fc
Haru02w <haru02w@protonmail.com>
2024-09-09 09:15:33 -0300

    add other steps

--- hosts/inspiron/nixos/deploy.sh
+++ hosts/inspiron/nixos/deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- -m disko --arg device '"/dev/sda"' ./disko.nix && \
+sudo nixos-generate-config --no-filesystems --root /mnt --show-hardware-config > ./hardware-configuration.nix && \
+git add ./hardware-configuration.nix && \
 yes | sudo nixos-install --root /mnt --flake .#inspiron
