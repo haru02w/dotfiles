a8935818a247d94ad1b0c248ecae86856860d054
Haru02w <haru02w@protonmail.com>
2024-09-12 16:23:56 -0300

    small change

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -77,13 +78,13 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # TODO:
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
     };
     nur.url = "github:nix-community/NUR";
-    # TODO:
   };
 
   nixConfig = {
