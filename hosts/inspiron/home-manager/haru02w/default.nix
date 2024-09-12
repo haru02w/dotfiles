342b937792b2b6f6cbbb4570c2b351a984a38ad0
Haru02w <haru02w@protonmail.com>
2024-09-12 17:00:54 -0300

    forget it

--- hosts/inspiron/home-manager/haru02w/default.nix
+++ hosts/inspiron/home-manager/haru02w/default.nix
@@ -1,29 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   imports = [./essentials.nix];
   programs.git = {
     enable = true;
     userName = "Haru02w";
     userEmail = "haru02w@protonmail.com";
-  };
-
-  stylix = {
-    enable = true;
-    image = ../../../../wallpapers/localhost.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark-terminal.yaml";
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-    };
-    fonts = {
-      monospace = {
-        package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
-        name = "FiraCode Nerd Font Mono";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
   };
 
   home.packages = [
