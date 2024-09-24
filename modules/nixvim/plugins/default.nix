11e97de9ca627dc42cf820e5f9d5dea66099dfe6
Haru02w <haru02w@protonmail.com>
2024-09-24 00:39:46 -0300

    let's sleep

--- modules/nixvim/plugins/default.nix
+++ modules/nixvim/plugins/default.nix
@@ -4,6 +4,7 @@
     ./treesitter/treesitter.nix # syntax highlight, folding, etc
     ./treesitter/treesitter-context.nix # show context of code when not visible
     ./treesitter/treesitter-textobjects.nix # select tokens based on the syntax
+    ./treesitter/ts-autotag.nix # auto-close html tags
 
     # lsp
     ./lsp/lsp.nix
@@ -11,21 +12,28 @@
     ./lsp/trouble.nix # TODO:
 
     # cmp
-    ./cmp/cmp.nix
-    ./cmp/lspkind.nix
+    ./cmp/autopairs.nix # add closing chars automatically
+    ./cmp/cmp.nix # completion
+    ./cmp/lspkind.nix # cool icons
 
+    # dap
+    # BUG: ./dap/dap.nix
+
+    # movement
     ./movement/flash.nix # TODO: fast navigation
-    ./movement/grapple.nix # fast file switcher
+    ./movement/grapple.nix # fast file switcher (harpoon alternative)
     # BUG: ./movement/harpoon.nix # fast file switcher
 
     # ui
-    ./ui/lualine.nix
+    ./ui/lualine.nix # line
     ./ui/alpha.nix
     ./ui/dressing.nix
     ./ui/indent-blankline.nix
     ./ui/noice.nix
     ./ui/precognition.nix
     ./ui/smart-splits.nix
+    ./ui/web-devicons.nix
+    ./ui/todo-comments.nix
 
     # extra
     ./extra/colorizer.nix # hex-color shows color
@@ -39,5 +47,7 @@
     ./extra/oil.nix # file manager
     ./extra/spectre.nix # search replace workdir
     ./extra/ufo.nix # folding
+    ./extra/hardtime.nix # enforce good pratices
+    ./extra/presence.nix # discord presence
   ];
 }
