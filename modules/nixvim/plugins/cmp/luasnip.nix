ae424c3bb1b80ddca9c03eb7cddcda8269c123e4
Haru02w <haru02w@protonmail.com>
2024-09-26 00:02:42 -0300

    fix some snippets weirdnesses

--- modules/nixvim/plugins/cmp/luasnip.nix
+++ modules/nixvim/plugins/cmp/luasnip.nix
@@ -3,7 +3,10 @@
     enable = true;
     settings = {
       enable_autosnippets = true;
-      store_selection_keys = "<Tab>";
+      store_selection_keys = "<C-l>";
+      history = false;
+      delete_check_events = "InsertEnter";
+      region_check_events = "InsertLeave";
     };
     fromVscode = [{
       lazyLoad = true;
