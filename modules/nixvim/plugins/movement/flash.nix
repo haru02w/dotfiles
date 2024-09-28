caee63bb0e7514acd84d40829e48b8f8db0428db
Haru02w <haru02w@protonmail.com>
2024-09-28 00:47:22 -0300

    more plugins

--- modules/nixvim/plugins/movement/flash.nix
+++ modules/nixvim/plugins/movement/flash.nix
@@ -1,3 +1,49 @@
-{}
+{ lib, ... }:
+with lib.nixvim; {
+  plugins.flash = {
+    enable = true;
+    settings = {
+      jump.autojump = true;
+      modes.search.enabled = true;
+    };
+  };
+  keymaps = [
+    {
+      key = "s";
+      mode = [ "n" "x" "o" ];
+      action = mkRaw "require('flash').jump";
+      options = {
+        silent = true;
+        desc = "flash";
+      };
+    }
+    {
+      key = "S";
+      mode = [ "n" "x" "o" ];
+      action = mkRaw "require('flash').treesitter";
+      options = {
+        silent = true;
+        desc = "flash";
+      };
+    }
+    {
+      key = "r";
+      mode = "o";
+      action = mkRaw "require('flash').remote";
+      options = {
+        silent = true;
+        desc = "flash";
+      };
+    }
+    {
+      key = "R";
+      mode = [ "o" "x" ];
+      action = mkRaw "require('flash').treesitter_search";
+      options = {
+        silent = true;
+        desc = "flash";
+      };
+    }
+  ];
+}
 # TODO:
-
