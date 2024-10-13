f119f5db750bd097c50ea832fc5654975df3d472
Haru02w <haru02w@protonmail.com>
2024-10-13 14:23:52 -0300

    add NAS host and all it's quirks

--- hosts/nas/nixos/setup/default.nix
+++ hosts/nas/nixos/setup/default.nix
@@ -3,7 +3,16 @@
     ./hardware-configuration.nix
   ];
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
   system.stateVersion = "24.05";
+
+  fileSystems."/var/lib/nextcloud" = {
+    device = "/dev/disk/by-uuid/2f606808-3cab-4c09-bd24-dd2624c92c51";
+    fsType = "btrfs";
+    options = ["auto" "nofail"];
+  };
 
   #ignore lid close
   services.logind.extraConfig = ''
--- modules/nixos/presets/nas-v1/default.nix
+++ modules/nixos/presets/nas-v1/default.nix
@@ -9,8 +9,9 @@ with lib; let
 in {
   options.modules.presets.nas-v1 = {
     enable = mkEnableOption "nas-v1 preset";
-    inherit (options.services.nextcloud.config) adminpassFile;
-    inherit (options.services.nextcloud) hostName;
+    adminpassFile = mkOption {
+      type = types.str;
+    };
     cloudflaredCredentialsFile = mkOption {
       type = types.str;
     };
@@ -23,9 +24,12 @@ in {
         package = pkgs.nextcloud30;
         https = true;
         configureRedis = true;
-        maxUploadSize = "1G";
-        inherit (cfg) hostName;
-        config.adminpassFile = cfg.adminpassFile;
+        database.createLocally = true;
+        maxUploadSize = "16G";
+        hostName = "nc.haru02w.eu.org";
+        #notify_push.enable = true;
+        autoUpdateApps.enable = true;
+        extraAppsEnable = true;
         extraApps = {
           inherit
             (config.services.nextcloud.package.packages.apps)
@@ -36,24 +40,43 @@ in {
             onlyoffice
             bookmarks
             twofactor_webauthn
-            notify_push
             previewgenerator
             ;
         };
-        extraAppsEnable = true;
+        settings = {
+          overwriteprotocol = "https";
+          default_phone_region = "BR";
+        };
+        config = {
+          dbtype = "pgsql";
+          adminuser = "haru02w";
+          inherit (cfg) adminpassFile;
+        };
       };
-      nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-        forceSSL = true;
-        enableACME = true;
+      onlyoffice = {
+        enable = true;
+        hostname = "oo.haru02w.eu.org";
+      };
+
+      nginx.virtualHosts = {
+        "${config.services.nextcloud.hostName}" = {
+          enableACME = true;
+          forceSSL = true;
+        };
+        "${config.services.onlyoffice.hostname}" = {
+          enableACME = true;
+          forceSSL = true;
+        };
       };
 
       cloudflared = {
         enable = true;
         tunnels = {
           "caf094a2-8ea9-4a78-a97d-041d3bbc1357" = {
+            originRequest.noTLSVerify = true;
             credentialsFile = cfg.cloudflaredCredentialsFile;
             default = "http_status:404";
-            ingress."${config.services.nextcloud.hostName}" = "https://localhost:443";
+            ingress."*.haru02w.eu.org" = "https://localhost";
           };
         };
       };
@@ -63,11 +86,10 @@ in {
         useRoutingFeatures = "server";
       };
     };
+
     security.acme = {
       acceptTerms = true;
-      certs = {
-        ${config.services.nextcloud.hostName}.email = "haru02w@protonmail.com";
-      };
+      defaults.email = "haru02w@protonmail.com";
     };
 
     environment.systemPackages = with pkgs; [
